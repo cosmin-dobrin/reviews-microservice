f3599f04d8daeae7abf455ea4e398d8112959316
cosmin <cosmin.dobrin17@gmail.com>
2023-01-14 20:10:06 +0200

    First commit.

--- src/main/java/com/bsd/reviewsmicroservice/controller/ReviewController.java
+++ src/main/java/com/bsd/reviewsmicroservice/controller/ReviewController.java
@@ -7,12 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -21,7 +15,6 @@ import java.util.List;
 public class ReviewController {
 
     private final ReviewService reviewService;
-
     private final ReviewTranslator reviewTranslator;
 
     @PostMapping
@@ -43,29 +36,4 @@ public class ReviewController {
         reviewService.deleteReview(reviewId);
         return ResponseEntity.ok().build();
     }
-
-    //    @GetMapping("/by-accommodation")
-//    public ResponseEntity<List<ReviewDto>> getReviewsByAccommodation(@RequestParam Long accommodationId) {
-//        return ResponseEntity.ok(
-//                reviewTranslator.generateReviewDtoList(
-//                        reviewService.getReviewsByAccommodation(accommodationId)));
-//    }
 }
-
-
-//        URI uri = UriComponentsBuilder.fromHttpUrl("localhost:8080/users/" + userId)
-//                                      .build()
-//                                      .toUri();
-//
-//        HttpRequest request = HttpRequest.newBuilder()
-//                                         .uri(uri)
-//                                         .method("GET", HttpRequest.BodyPublishers.noBody())
-//                                         .build();
-
-//        HttpResponse<String> response = HttpClient.newHttpClient()
-//                                                  .send(request, HttpResponse.BodyHandlers.ofString());
-//        System.out.println(response.body());
-
-//        JSONArray income = JsonPath.parse(response.body()).read("$.");
-
-//        System.out.println(income);
